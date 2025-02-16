95719782a9904102b317f18a2d4ef73085a3a557
kawukiandrew8888 <118190111+kawukiandrew8888@users.noreply.github.com>
2025-02-16 11:36:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def health_check():
    return 'OK', 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port)
